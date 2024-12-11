8bcb10069dcf45967e245fd654b8756f3bfad867
JosephBean <kk1593@naver.com>
2024-12-11 16:28:38 +0900

    메인 화면 영상 링크 추가

--- Home.py
+++ Home.py
@@ -10,22 +10,32 @@ st.set_page_config(
 # st.title("프로젝트 목록")
 st.markdown("<h1 style='text-align: center;'>프로젝트 목록</h1>", unsafe_allow_html=True)
 
-col1, col2 = st.columns(2)
+st.subheader("1. 가구원수 비교")
+with st.expander("시연 영상 보기"):
+    st.video("./mp4/team5.mp4")
+    st.page_link(page="./pages/1_가구원수 통계.py", label="[그래프 보기]", icon="🔗")
 
-col1.header("1. 가구원수 비교")
-col1.page_link(page="./pages/1_가구원수 통계.py", label="[그래프 보기]", icon="🔗")
+st.subheader("2. 뮤지컬 실시간 통계 차트")
+with st.expander("시연 영상 보기"):
+    st.video("./mp4/team5.mp4")
+    st.page_link(page="./pages/2_뮤지컬 실시간 통계.py", label="[차트 보기]", icon="🔗")
 
-col1.header("2. 뮤지컬 실시간 통계 차트")
-col1.page_link(page="./pages/2_뮤지컬 실시간 통계.py", label="[차트 보기]", icon="🔗")
+st.subheader("3. 암환자수 통계 그래프")
+with st.expander("시연 영상 보기"):
+    st.video("./mp4/team5.mp4")
+    st.page_link(page="./pages/3_암환자수 통계.py", label="[그래프 보기]", icon="🔗")
 
-col1.header("3. 암환자수 통계 그래프")
-col1.page_link(page="./pages/3_암환자수 통계.py", label="[그래프 보기]", icon="🔗")
+st.subheader("4. 베스트셀러 비교")
+with st.expander("시연 영상 보기"):
+    st.video("./mp4/team5.mp4")
+    st.page_link(page="./pages/4_베스트셀러 통계.py", label="[그래프 보기]", icon="🔗")
 
-col2.header("4. 베스트셀러 비교")
-col2.page_link(page="./pages/4_베스트셀러 통계.py", label="[그래프 보기]", icon="🔗")
+st.subheader("5. 티켓 비교 그래프")
+with st.expander("시연 영상 보기"):
+    st.video("./mp4/team5.mp4")
+    st.page_link(page="./pages/5_티켓 통계.py", label="[그래프 보기]", icon="🔗")
 
-col2.header("5. 티켓 비교 그래프")
-col2.page_link(page="./pages/5_티켓 통계.py", label="[그래프 보기]", icon="🔗")
-
-col2.header("6. 음악사이트 장르별인기 차트 그래프")
-col2.page_link(page="./pages/6_음악사이트 장르별 인기 통계.py", label="[차트, 그래프 보기]", icon="🔗")
+st.subheader("6. 음악사이트 장르별인기 차트 그래프")
+with st.expander("시연 영상 보기"):
+    st.video("./mp4/team5.mp4")
+    st.page_link(page="./pages/6_음악사이트 장르별 인기 통계.py", label="[차트, 그래프 보기]", icon="🔗")
